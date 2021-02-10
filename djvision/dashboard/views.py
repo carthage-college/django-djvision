90af7e1da56f9abd35d87444e0cf4a0b46c9d999
plungerman <plungerman@gmail.com>
2021-02-10 15:52:21 -0600

    now using onelogin for ldap

--- djvision/dashboard/views.py
+++ djvision/dashboard/views.py
@@ -1,18 +1,17 @@
 # -*- coding: utf-8 -*-
+#
 from django.conf import settings
-from django.shortcuts import render
 from django.core.urlresolvers import reverse_lazy
-
+from django.shortcuts import render
+from djauth.decorators import portal_auth_required
 from djvision.core.data import ProvisioningDetailRec
 from djvision.dashboard.forms import DetailCreatedForm
-
-from djzbar.decorators.auth import portal_auth_required
 from djzbar.utils.informix import get_session
 
 
 @portal_auth_required(
     group='LIS', session_var='DJVISION_AUTH',
-    redirect_url=reverse_lazy('access_denied')
+    redirect_url=reverse_lazy('access_denied'),
 )
 def home(request):
     error = None
@@ -23,13 +22,13 @@ def home(request):
             data = form.cleaned_data
             session = get_session(settings.INFORMIX_EARL)
             objects = session.query(ProvisioningDetailRec).filter(
-                ProvisioningDetailRec.created_at >= data['created_at']
+                ProvisioningDetailRec.created_at >= data['created_at'],
             ).all()
     else:
         form = DetailCreatedForm()
 
     return render(
         request, 'dashboard/home.html', {
-            'form':form, 'objects':objects, 'error':error
+            'form': form, 'objects': objects, 'error': error,
         }
     )
